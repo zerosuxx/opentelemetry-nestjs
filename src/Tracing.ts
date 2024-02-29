ff2d831290fe8a51882a5065064009e9f5c28961
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-02-29 08:29:06 +0000

    chore: update semantic-release dependency

--- src/Tracing.ts
+++ src/Tracing.ts
@@ -1,9 +1,13 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OpenTelemetryModuleConfig } from './OpenTelemetryModuleConfig.interface';
+import { OpenTelemetryModuleDefaultConfig } from './OpenTelemetryModuleDefaultConfig';
 
 export class Tracing {
   static init(configuration: OpenTelemetryModuleConfig): void {
-    const otelSDK = new NodeSDK(configuration);
+    const otelSDK = new NodeSDK({
+      ...OpenTelemetryModuleDefaultConfig,
+      ...configuration,
+    });
     otelSDK.start();
   }
 }
